79b96367c2d5339376c5d428be503ac23fbd3863
stmigarcia <stmigarcia@gmail.com>
2018-08-21 09:03:30 -0600

    Update status_curl.sh

--- status_curl.sh
+++ status_curl.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #while statements to curl status of carmen if there are new transactions
 
-while true; do ./stevescript.sh >> carmen_results.csv  ; sleep 20; done
+while true; 
+do ./stevescript.sh >> carmen_results.csv
+sleep 20
+done
