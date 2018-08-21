d8c0713b5b6bdde53179f027cced79ca25b28233
Steve Garcia <steve@blinker.com>
2018-08-21 13:27:12 -0600

    adding a function to look for and/or create .csv

--- all-together-now.sh
+++ all-together-now.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # statement to grab a curl of a server, append it to a csv file
+if [ -f /tmp/carmen_results.csv ]; 
+then
+	echo "File not found"
+	else	
+		touch /tmp/carmen_results.csv
+fi		
+
 while true;
 do
-./total_requests.sh < carmen_results.csv
+./total_requests.sh < /tmp/carmen_results.csv
 
 #if statement to exclude known total value
 
-if cat carmen_results.csv | grep -Ev 2561 | grep -Ev 2799 | grep -Ev 1154 | grep -Ev 1117;
+if cat /tmp/carmen_results.csv | grep -Ev 2561 | grep -Ev 2799 | grep -Ev 1154 | grep -Ev 1117;
 then
   echo no new change;
 fi
